fe01938cbcb3c2f6b3c047047b953ed43763d7b6
rus152 <77641121+rus152@users.noreply.github.com>
2025-02-03 05:06:40 +0400

    Обновить .gitignore и изменить путь к файлу, промпт выведен в одельный файл. Результат записывается в файл. Если програме кажется что, что-то не так, он выдаёт ошибку.

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-from langchain_community.document_loaders import UnstructuredExcelLoader
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
+from datetime import datetime
+import os
 
-# Задаем путь к Excel-файлу и загружаем его содержимое
-excel_path = "Checklist.xlsx"
-loader = UnstructuredExcelLoader(excel_path, mode="elements")  # или mode="single" для одного элемента
-documents = loader.load()
 
-with open("5.txt", 'r', encoding='utf-8') as file:
+with open("res/5.txt", 'r', encoding='utf-8') as file:
     txt_content = file.read()
 
-
-# Объединяем текст из всех документов в один промпт
-excel_text = "\n".join(doc.page_content for doc in documents)
+with open("prompt.txt", 'r', encoding='utf-8') as file:
+    prompt = file.read()
 
 # Создаем модель ChatOpenAI
 model = ChatOpenAI(model="o3-mini")
 
 message = [
     SystemMessage(
-        content=f"У тебя есть Excel-файл с текстом. Вот его содержимое:\n\n{excel_text}\n\nТвоя задача оценить по данному чек листу насколько хорошо выполнена работа сотрудника больницы. Так же ты должен давать рекомендации врачу, что ему надо сделать что бы повысить свою оценку(Не трогать чек лист). Если есть какие то ошибки с субординации, при упоминание приводи цитату, где врач был неформален в общении. Формат ответа такой: \n\n Выполено критериев чеклиста:(Проценты выполеной работы)\n Допущенные ошибки:\n-(Перечисление ошибок через черту)\nРекомендации:\n-(Перечисление рекомендаций через черту)\n\Финальная оценка: (Оценка по 10-бальной шкале)\n\n",
+        content=prompt,
         sender="system",
     ),
     HumanMessage(content=txt_content, sender="human"),
@@ -27,4 +23,17 @@ message = [
 # Отправляем текст Excel-файла в модель
 result = model.invoke(message)
 
+if (result.content == "Error. The text is not a dialogue."):
+    print("Ошибка, удостоверьтесь, что текст является диалогом.\n")
+    exit()
+elif (result.content == "Error. The dialog doesn't look right."):
+    print("Ошибка, диалог не выглядит правильным.\n")
+    exit()
+else:
+    os.makedirs('results', exist_ok=True)
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    with open(f'results/response_{timestamp}.json', 'w', encoding='utf-8') as file:
+        file.write(result.content)
+
 print(result.content)
+
